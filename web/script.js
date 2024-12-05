0682f0dfab5955e0f9ca9e40b8bb72873ea15b47
bottledkzk <3226535826@qq.com>
2024-12-05 19:48:52 +0800

    Update script.js

--- web/script.js
+++ web/script.js
@@ -13,6 +13,33 @@ const images = [
 
     '../images/hust6.jpg',
 
+    '../images/hust7.jpg',
+
+    '../images/hust8.jpg',
+
+    '../images/hust9.jpg',
+
+    '../images/hust10.jpg',
+
+    '../images/hust11.jpg',
+
+    '../images/hust12.jpg',
+
+    '../images/hust13.jpg',
+
+    '../images/hust14.jpg',
+
+    '../images/hust15.jpg',
+
+    '../images/hust16.jpg',
+
+    '../images/hust17.jpg',
+
+    '../images/hust18.jpg',
+
+    '../images/hust19.jpg',
+
+
 ];
 
 const randomIndex = Math.floor(Math.random() * images.length);
